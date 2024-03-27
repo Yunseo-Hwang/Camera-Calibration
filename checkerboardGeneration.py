342bd82d722f841d6540f064197b42840b6df46a
Yunseo Hwang <85391489+Yunseo-Hwang@users.noreply.github.com>
2024-03-27 14:41:53 -0400

    Capture frames every 1 seconds

--- checkerboardGeneration.py
+++ checkerboardGeneration.py
@@ -1,7 +1,7 @@
 import cv2
 import os
 
-# Path to input video
+# Path to the input video
 video_path = 'checkerboard.mp4'
 
 # Directory to save extracted images
@@ -16,11 +16,24 @@ criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 # Open the video file
 cap = cv2.VideoCapture(video_path)
 
-frame_count = 0
+# Get the frame rate of the video
+fps = cap.get(cv2.CAP_PROP_FPS)
+
+# Calculate the number of frames to skip for capturing every 0.5 seconds
+skip_frames = int(fps * 1)
+
+frame_count = -1
 while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
         break
+
+    # Increment frame count
+    frame_count += 1
+
+    # Skip frames if necessary
+    if frame_count % skip_frames != 0:
+        continue
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
@@ -34,8 +47,6 @@ while cap.isOpened():
         image_name = f'image_{frame_count:04d}.jpg'
         cv2.imwrite(os.path.join(output_dir, image_name), frame)
         print(f'Saved {image_name}')
-
-    frame_count += 1
 
 cap.release()
 cv2.destroyAllWindows()
